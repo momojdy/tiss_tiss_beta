6c9ceff1795e6b94041e16d279ff0561117b4e39
momojdy <59683856+momojdy@users.noreply.github.com>
2026-09-25 01:36:48 +0800

    Fix App font import syntax

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import {StyleSheet} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {useFonts} from '@expo-google-fonts/montserrat';
-import {Montserrat_400Regular,Montserrat_500Medium,Montserrat_600SemiBold,Montserrat_700Bold,Montserrat_400Regular_Italic} from '@expo-google-fonts/montserrat';
+import {useFonts,Montserrat_400Regular,Montserrat_500Medium,Montserrat_600SemiBold,Montserrat_700Bold,Montserrat_400Regular_Italic} from '@expo-google-fonts/montserrat';
 import MainHomePage from './src/screens/MainHomePage';
 
 export default function App(){
